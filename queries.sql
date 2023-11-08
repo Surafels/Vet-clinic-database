eea53e6bd411fec5fbad607d54f54059e5758560
Surafels <ssurafel4@gmail.com>
2023-11-08 16:39:57 +0300

    Update READEME

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 
@@ -10,7 +10,7 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
-SELECT * FROM animals WHERE neutered =  true;
+SELECT * FROM animals WHERE neutered =  true;S
 
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
