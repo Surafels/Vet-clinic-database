efbf0e888a5744e83d9a54a7e60ff2ffc3504f6a
Surafels <ssurafel4@gmail.com>
2023-11-09 15:27:14 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -15,3 +15,46 @@ SELECT * FROM animals WHERE neutered =  true;S
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN  10.4 and 17.3;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Start a new transaction
+BEGIN;
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO SAVEPOINT my_savepoint;
+
+-- Update weights of negative animals to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+
+SELECT COUNT(*) AS total_animals FROM animals;
+
+SELECT COUNT(*) AS animals_never_escaped
+FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) AS average_weight
+FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+
+SELECT animal_type, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY animal_type;
+
+SELECT animal_type, AVG(escape_attempts) AS average_escape_attempts
+FROM animals
+WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY animal_type;
