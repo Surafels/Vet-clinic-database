ac1d60505b256b44105e504f60a04f087c3b4d33
Surafels <ssurafel4@gmail.com>
2023-11-10 16:04:09 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE animals (
     weight_kg DECIMAL(10, 2),
     species VARCHAR(255)
 );
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
