9e17ce79f94ded8cf352a81d64d3f34f9d0a8216
Surafels <ssurafel4@gmail.com>
2023-11-10 18:33:48 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -107,6 +107,38 @@ FROM animals
 WHERE birth_year BETWEEN 1990 AND 2000
 GROUP BY species;
 
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT owners.full_name, animals.name
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id;
+
+SELECT species.name, COUNT(animals.name) AS animal_count
+FROM species
+LEFT JOIN animals ON species.id = animals.species_id
+GROUP BY species.name;
+
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+JOIN species ON animals.species_id = species.id
+WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+LEFT JOIN escapes ON animals.id = escapes.animal_id
+WHERE owners.full_name = 'Dean Winchester' AND escapes.animal_id IS NULL;
+
+SELECT owners.full_name, COUNT(animals.name) AS animal_count
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY animal_count DESC
+LIMIT 1;
 
 
 
